fe0a0653b56e7a4e3c9f025c32359942e0f22f54
Prateek Saxena <prtksxna@gmail.com>
2015-01-09 16:20:29 +0530

    Merge Timo's changes
    https://en.wikipedia.org/w/index.php?title=User%3AKrinkle%2Ftoc.js&diff=640305673&oldid=640198701
    https://en.wikipedia.org/w/index.php?title=User%3AKrinkle%2Ftoc.css&diff=640306534&oldid=640305495
    
    Closes #2

--- ptoc.js
+++ ptoc.js
@@ -1,49 +1,62 @@
-( function ( $ ) {
+mw.loader.using( 'jquery.throttle-debounce', function () {
+	$( function () {
+		var $window, $mwPanel, $floatTOC, scrollHandler,
+			tocLimit, headingOffsets,
+			$toc = $( '#toc' );
 
-    $( function () {
-	var $toc = $( '#toc' ),
-	    $window = $( window ),
-	    $mwPanel = $( '#mw-panel' ),
-	    tocLimit = $toc.offset().top + $toc.height(),
-	    $floatTOC = $toc
-	    .clone()
-	    .attr( 'id', '')
-	    .addClass( 'floatTOC' )
-	    .appendTo( 'body' )
-	    .css( 'height', $window.height() )
-	    .hide(),
-	    headingsOffset = [];
+		if ( !$toc.length ) {
+			return;
+		}
 
-	// Get all heading positions
-	$('.mw-headline').each( function() {
-	    headingsOffset.push( [ $(this).attr( 'id' ), $( this ).offset().top ] );
-	} );
+		$window = $( window );
+		$mwPanel = $( '#mw-panel' );
+		$floatTOC = $toc
+			.clone()
+			.removeAttr( 'id' )
+			.addClass( 'floatTOC' )
+			.appendTo( 'body' )
+			.hide();
 
-	// For the window scroll event
-	var scroller = function () {
-	    var scrollTop = $window.scrollTop();
-	    if ( scrollTop > tocLimit ) {
-		$floatTOC.show();
-		$mwPanel.hide();
+		tocLimit = $toc.offset().top + $toc.height();
+		headingOffsets = [];
 
-		// Highlight current
-		$floatTOC.find( 'a' ).css( 'font-weight', 'normal' )
-		var highlight = "";
-		$.each( headingsOffset, function ( i, v ) {
-		    if ( scrollTop < v[ 1 ]) {
-			highlight = headingsOffset[ i -1 ][ 0 ];
-			return false;
-		    }
+		// Get all heading positions
+		$('.mw-headline').each( function () {
+			headingOffsets.push( [ $( this ).attr( 'id' ), $( this ).offset().top ] );
 		} );
-		$floatTOC.find( 'a[href="#' + highlight + '"]' ).css( 'font-weight', 'bold' );
 
-	    } else {
-		$floatTOC.hide();
-		$mwPanel.show();
-	    }
-	}
+		// For the window scroll event
+		scrollHandler = function () {
+			var $current,
+			scrollTop = $window.scrollTop();
 
-	$window.on( 'scroll', $.throttle( 250, scroller ) );
-    } );
+			if ( scrollTop > tocLimit ) {
+				$floatTOC.show();
+				$mwPanel.hide();
 
-} ) ( jQuery );
+				// Highlight current
+				var highlight = false;
+				// Current section is above the first heading below the top of the screen
+				$.each( headingOffsets, function ( i, v ) {
+					// Skip first as there's no previous heading before the first
+					if ( i !== 0 && scrollTop < v[ 1 ] ) {
+						highlight = headingOffsets[ i - 1 ][ 0 ];
+						return false;
+					}
+				} );
+
+				if ( highlight ) {
+					$current = $floatTOC.find( 'a[href="#' + highlight + '"]' );
+					$floatTOC.find( 'a' ).not( $current ).css( 'font-weight', '' );
+					$current.css( 'font-weight', 'bold' );
+				}
+
+			} else {
+				$floatTOC.hide();
+				$mwPanel.show();
+			}
+		}
+
+		$window.on( 'scroll', $.throttle( 250, scrollHandler ) );
+	} );
+} );
